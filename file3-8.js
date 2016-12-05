057b3e12eb527e4389e810d79d03907e8b12df1c
Saeko Fukui <saeko0032@gmail.com>
2016-12-05 00:08:29 -0800

    example3-8
    functions, return and if/ else

---
+++ file3-8.js
@@ -0,0 +1,10 @@
+// Define quarter here.
+var quarter = function(number) {
+    return number / 4;
+};
+
+if (quarter(120) % 3 === 0 ) {
+  console.log("The statement is true");
+} else {
+  console.log("The statement is false");
+}
