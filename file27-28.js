904415d4d6a7cf0f8ef6448b7ca10869d9298081
Saeko Fukui <saeko0032@gmail.com>
2016-12-04 21:46:31 -0800

    example27and28
    conclusion

---
+++ file27-28.js
@@ -0,0 +1,8 @@
+var myColor = "blue";
+console.log(myColor.length);
+// Not sure where to begin? Check the Hint!
+if (12%2 === 0) {
+    console.log("I finished my first course!");
+} else {
+    console.log("woops!something wrong with it");
+}
