844321f7041bce06723ad9c0532cecf85524cff1
Saeko Fukui <saeko0032@gmail.com>
2016-12-04 21:37:51 -0800

    example24

---
+++ file24.js
@@ -0,0 +1,7 @@
+// To create a variable, we use only one equals sign
+// But to check if two values are equal, we use 3 equal signs.
+
+// declare your variable here:
+var myAge = 25;
+
+console.log(myAge);
