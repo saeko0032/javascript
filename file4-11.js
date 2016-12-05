aa3f317b9e998aa03d8fc496ecc9b1b3e41a294a
Saeko Fukui <saeko0032@gmail.com>
2016-12-05 00:55:40 -0800

    example4-11
    loops and arrays

---
+++ file4-11.js
@@ -0,0 +1,7 @@
+// Let's print out every element of an array using a for loop
+
+var cities = ["Melbourne", "Amman", "Helsinki", "NYC", "Halifax"];
+
+for (var i = 0; i < cities.length; i++) {
+    console.log("I would like to visit " + cities[i]);
+}
