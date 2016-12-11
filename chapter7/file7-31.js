8d91e5f82d383b2679fa4e30c66a8d2f2a8c8e52
Saeko Fukui <saeko0032@gmail.com>
2016-12-10 23:31:19 -0800

    example7-31

---
+++ chapter7/file7-31.js
@@ -0,0 +1,16 @@
+var snoopy = new Object();
+snoopy.species = "beagle";
+snoopy.age = 10;
+
+// save Snoopy's age and species into variables
+// use dot notation for snoopy's species
+var species = snoopy.species;
+    
+// use bracket notation for snoopy's age
+var age = snoopy["age"];
+
+// Dot notation, with 
+// ObjectName.PropertyName
+
+// Bracket notation, with 
+// ObjectName["PropertyName"] (don't forget the quotes!)
