afcd146cf0d09d96a0b2b12897cb7c5ee0d06afd
Saeko Fukui <saeko0032@gmail.com>
2016-12-16 09:58:48 -0800

    javascript class

---
+++ chapter8/file8-11.js
@@ -0,0 +1,16 @@
+function Person(name,age) {
+  this.name = name;
+  this.age = age;
+}
+// a function that prints the name of any given person
+var printPersonName = function (p) {
+  console.log(p.name);
+};
+
+var bob = new Person("Bob Smith", 30);
+printPersonName(bob);
+
+// make a person called me with your name and age
+// then use printPersonName to print your name
+var me = new Person("Saeko", 30);
+printPersonName(me);
